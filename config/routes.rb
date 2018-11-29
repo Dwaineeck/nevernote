a88a671d49d41e1dfcd68f7946663abc98de2f81
YOUR NAME <kiethechols@outlook.com>
2018-11-28 22:10:24 -0600

    Doc controller and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # root directory
-  root 'welcome#index' 
+  root 'welcome#index'
+
+  # create all the routes for different actions for our application
+  resources :docs
 end
