c3709b43f201d785f3162787db5f103c9bd496d4
YOUR NAME <kiethechols@outlook.com>
2018-11-28 23:12:22 -0600

    change root of application for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  # root directory
-  root 'welcome#index'
 
   # create all the routes for different actions for our application
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+  # root directory
+  root 'welcome#index'
+
+
 end
