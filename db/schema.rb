436cd3e0490d2236ed045ac41746c9e4521c85b3
YOUR NAME <kiethechols@outlook.com>
2018-12-03 20:29:29 -0600

    updated gem files

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_050007) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
